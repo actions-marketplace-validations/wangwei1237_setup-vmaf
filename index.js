d3ff1dfe44c822d87e6b7e3f70c62ea038892d3d
Wang Wei <wangwei1237@gmail.com>
2021-04-19 17:57:37 +0800

    init

--- index.js
+++ index.js
@@ -9,7 +9,7 @@ async function run() {
       const nameToGreet = core.getInput('who-to-greet');
       console.log(`Hello ${nameToGreet}!`);
       const time = (new Date()).toTimeString();
-      core.setOutput("time", time);
+      core.setOutput("time", time + " " + nameToGreet);
       // Get the JSON webhook payload for the event that triggered the workflow
       const payload = JSON.stringify(github.context.payload, undefined, 2)
       console.log(`The event payload: ${payload}`);
