c845a7abd8dd9aba6d3746134320c6e6a48dabe6
Wang Wei <wangwei1237@gmail.com>
2021-04-19 20:02:25 +0800

    init

--- index.js
+++ index.js
@@ -33,7 +33,7 @@ async function run() {
       const vmafPrefixPath = './libvmaf';
       const vmafBuildPath  = './vmaf/libvmaf/build';
       core.startGroup('Compile and install');
-      await exec.exec(`meson setup vmaf/libvmaf/build vmaf/libvmaf --prefix=$(pwd)/libvmaf`);
+      await exec.exec(`meson setup vmaf/libvmaf/build vmaf/libvmaf --prefix=/home/runner/work/setup-vmaf/setup-vmaf/libvmaf`);
       await exec.exec(`ninja -vC vmaf/libvmaf/build install`);
       await exec.exec(`ls -R .`);
       core.endGroup();
