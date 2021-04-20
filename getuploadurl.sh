04037533111b633d626dbdf117515a4df709b81b
Wang Wei <wangwei1237@gmail.com>
2021-04-20 19:53:01 +0800

    init

--- getuploadurl.sh
+++ getuploadurl.sh
@@ -8,15 +8,19 @@ fi
 URL_PREFIX="https://api.github.com/repos/wangwei1237/setup-vmaf/releases"
 
 version=$1
+token=$2
+
 tag="libvmaf-${version#v}"
 echo $tag
+echo $token
+
 get_release_url="${URL_PREFIX}/tags/${tag}"
 upload_url=$(curl -H "Accept: application/vnd.github.v3+json" "${get_release_url}" | grep 'upload_url' | cut -d'"' -f4)
 
 create_release_url="${URL_PREFIX}"
 if [ "$upload_url" = "" ]
 then
-    curl -H "Accept: application/vnd.github.v3+json" "${create_release_url}" -d "{\"tag_name\":\"${tag}\"}"
+    curl -X POST -H "Accept: application/vnd.github.v3+json" "${create_release_url}?access_token=${token}" -d "{\"tag_name\":\"${tag}\"}"
 fi
 
 echo "-----------${upload_url}"
