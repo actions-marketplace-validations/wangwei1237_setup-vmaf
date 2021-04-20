a896126ca8a8dda99539ae4286f2d589313e281b
Wang Wei <wangwei1237@gmail.com>
2021-04-20 20:02:06 +0800

    init

--- getuploadurl.sh
+++ getuploadurl.sh
@@ -11,8 +11,6 @@ version=$1
 token=$2
 
 tag="libvmaf-${version#v}"
-echo $tag
-echo $token
 
 get_release_url="${URL_PREFIX}/tags/${tag}"
 upload_url=$(curl -H "Accept: application/vnd.github.v3+json" "${get_release_url}" | grep 'upload_url' | cut -d'"' -f4)
@@ -20,9 +18,8 @@ upload_url=$(curl -H "Accept: application/vnd.github.v3+json" "${get_release_url
 create_release_url="${URL_PREFIX}"
 if [ "$upload_url" = "" ]
 then
-    curl -X POST -H "Accept: application/vnd.github.v3+json" "${create_release_url}?access_token=${token}" -d "{\"tag_name\":\"${tag}\"}"
+    upload_url=$(curl -X POST -H "Accept: application/vnd.github.v3+json" "${create_release_url}?access_token=${token}" -d "{\"tag_name\":\"${tag}\", \"name\":\"libvmaf Build for ${version}\"}" | grep 'upload_url' | cut -d'"' -f4)
 fi
 
 echo "-----------${upload_url}"
 echo "::set-output name=upload-url::${upload_url}"
-echo "::error Something went wrong"
